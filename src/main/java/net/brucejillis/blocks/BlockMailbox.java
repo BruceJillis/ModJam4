2f32ccfb32ddc344195a4a9e4fb1d7c5d62670b2
Jillis ter Hove <j.terhove@gmail.com>
2014-05-15 15:32:47 +0200

    autocommit day 1 15:32

--- src/main/java/net/brucejillis/blocks/BlockMailbox.java
+++ src/main/java/net/brucejillis/blocks/BlockMailbox.java
@@ -26,6 +26,12 @@ public class BlockMailbox extends BlockContainer {
     }
 
     @Override
+    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List pool, Entity entity) {
+        setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.925f, 1.0f);
+        super.addCollisionBoxesToList(world, x, y, z, aabb, pool, entity);
+    }
+
+    @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
         TileEntity te = world.getTileEntity(x, y, z);
         if (te == null) {
--- src/main/java/net/brucejillis/handlers/GuiMailbox.java
+++ src/main/java/net/brucejillis/handlers/GuiMailbox.java
@@ -1,27 +1,38 @@
 package net.brucejillis.handlers;
 
+import net.brucejillis.MailboxMod;
 import net.brucejillis.tileentities.TileEntityMailbox;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 public class GuiMailbox extends GuiScreen {
+    private ResourceLocation background = new ResourceLocation(MailboxMod.ID, "/textures/gui/mailbox.png");
+
     private final EntityPlayer player;
     private final TileEntityMailbox entity;
+
+    private int xSize = 0;
+    private int ySize = 0;
 
     public GuiMailbox(EntityPlayer player, TileEntityMailbox entity) {
         this.player = player;
         this.entity = entity;
-    }
-
-    @Override
-    public void initGui() {
+        xSize = 195;
+        ySize = 136;
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         drawDefaultBackground();
+        drawGUIBackground();
+    }
+
+    private void drawGUIBackground() {
+        mc.renderEngine.bindTexture(background);
+        drawTexturedModalRect((width - xSize)/2, (height - ySize)/2, 0, 0, xSize, ySize);
     }
 
     @Override
