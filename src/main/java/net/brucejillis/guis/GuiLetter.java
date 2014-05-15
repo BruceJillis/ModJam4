37ec2f9362e2c61aafc34b669f77bdad36535ba9
Jillis ter Hove <j.terhove@gmail.com>
2014-05-16 00:44:01 +0200

    autocommit day 0 00:44

--- src/main/java/net/brucejillis/guis/GuiLetter.java
+++ src/main/java/net/brucejillis/guis/GuiLetter.java
@@ -6,10 +6,15 @@ import net.brucejillis.containers.ContainerMailbox;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 public class GuiLetter extends GuiContainer {
     private ResourceLocation background = new ResourceLocation(MailboxMod.ID, "/textures/gui/letter.png");
@@ -28,11 +33,31 @@ public class GuiLetter extends GuiContainer {
     public void initGui() {
         super.initGui();
         Keyboard.enableRepeatEvents(true);
-        subject = new GuiTextField(this.fontRendererObj, guiLeft + 37, guiTop + 34, 133, 14);
+        subject = new GuiTextField(this.fontRendererObj, guiLeft + 37, guiTop + 34, 125, 12);
         subject.setTextColor(-1);
         subject.setDisabledTextColour(-1);
-        subject.setEnableBackgroundDrawing(false);
-        subject.setMaxStringLength(40);
+        subject.setMaxStringLength(45);
+    }
+
+    public void onGuiClosed() {
+        super.onGuiClosed();
+        Keyboard.enableRepeatEvents(false);
+    }
+
+    protected void keyTyped(char par1, int par2) {
+        this.subject.textboxKeyTyped(par1, par2);
+    }
+
+    protected void mouseClicked(int par1, int par2, int par3) {
+        super.mouseClicked(par1, par2, par3);
+        this.subject.mouseClicked(par1, par2, par3);
+    }
+
+    public void drawScreen(int par1, int par2, float par3) {
+        super.drawScreen(par1, par2, par3);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_BLEND);
+        this.subject.drawTextBox();
     }
 
     @Override
@@ -40,6 +65,42 @@ public class GuiLetter extends GuiContainer {
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         mc.renderEngine.bindTexture(background);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+    }
+
+    public static void renderEntityWithYawAndPitch(int x, int y, int scale, float yaw, float pitch, EntityLivingBase entity) {
+        GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float) x, (float) y, 50.0F);
+        GL11.glScalef((float) (-scale), (float) scale, (float) scale);
+        GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+        float f2 = entity.renderYawOffset;
+        float f3 = entity.rotationYaw;
+        float f4 = entity.rotationPitch;
+        float f5 = entity.prevRotationYawHead;
+        float f6 = entity.rotationYawHead;
+        GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(-((float) Math.atan((double) (pitch / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+        entity.renderYawOffset = (float) Math.atan((double) (yaw / 40.0F)) * 20.0F;
+        entity.rotationYaw = (float) Math.atan((double) (yaw / 40.0F)) * 40.0F;
+        entity.rotationPitch = -((float) Math.atan((double) (pitch / 40.0F))) * 20.0F;
+        entity.rotationYawHead = entity.rotationYaw;
+        entity.prevRotationYawHead = entity.rotationYaw;
+        GL11.glTranslatef(0.0F, entity.yOffset, 0.0F);
+        RenderManager.instance.playerViewY = 180.0F;
+        RenderManager.instance.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+        entity.renderYawOffset = f2;
+        entity.rotationYaw = f3;
+        entity.rotationPitch = f4;
+        entity.prevRotationYawHead = f5;
+        entity.rotationYawHead = f6;
+        GL11.glPopMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 
     @Override
