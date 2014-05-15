92665aab3f367a158532ed0b1f9442389938ac22
Jillis ter Hove <j.terhove@gmail.com>
2014-05-16 00:15:00 +0200

    autocommit day 0 00:15

---
+++ src/main/java/net/brucejillis/containers/ContainerLetter.java
@@ -0,0 +1,6 @@
+package net.brucejillis.containers;
+
+import net.minecraft.inventory.Container;
+
+public class ContainerLetter extends Container {
+}
--- src/main/java/net/brucejillis/guis/GuiLetter.java
+++ src/main/java/net/brucejillis/guis/GuiLetter.java
@@ -1,20 +1,20 @@
 package net.brucejillis.guis;
 
+import net.brucejillis.MailboxMod;
+import net.brucejillis.containers.ContainerLetter;
+import net.brucejillis.containers.ContainerMailbox;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 
-public class GuiLetter extends GuiScreen {
+public class GuiLetter extends GuiContainer {
+    private ResourceLocation background = new ResourceLocation(MailboxMod.ID, "/textures/gui/letter.png");
     private EntityPlayer player;
 
     public GuiLetter(EntityPlayer player) {
+        super(new ContainerLetter(player.inventory, ));
         this.player = player;
-    }
-
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partial) {
-        super.drawScreen(mouseX, mouseY, partial);
-        drawDefaultBackground();
-
     }
 
     @Override
