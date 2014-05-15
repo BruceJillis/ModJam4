1c3ca6808e2b8cbacd70e12302db488c5d4ce425
Jillis ter Hove <j.terhove@gmail.com>
2014-05-15 23:31:30 +0200

    autocommit day 1 23:31

--- src/main/java/net/brucejillis/items/ItemLetter.java
+++ src/main/java/net/brucejillis/items/ItemLetter.java
@@ -1,13 +1,21 @@
 package net.brucejillis.items;
 
 import net.brucejillis.MailboxMod;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemLetter extends Item {
     public ItemLetter() {
         setUnlocalizedName("itemLetter");
-        setTextureName(MailboxMod.ID + ":" + getUnlocalizedName().substring(5));
+        setTextureName(MailboxMod.ID + ":" + getUnlocalizedName().substring(5) + "_blank");
         setCreativeTab(MailboxMod.mailboxTab);
         setMaxStackSize(16);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+
     }
 }
