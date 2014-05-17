020f20c5e5b6446428b7da353b853239268f4acf
Jillis ter Hove <j.terhove@gmail.com>
2014-05-17 19:42:12 +0200

    autocommit day -1 19:42

--- src/main/java/net/brucejillis/guis/GuiLetter.java
+++ src/main/java/net/brucejillis/guis/GuiLetter.java
@@ -3,6 +3,7 @@ package net.brucejillis.guis;
 import net.brucejillis.MailboxMod;
 import net.brucejillis.containers.ContainerLetter;
 import net.brucejillis.handlers.packets.PacketChangeInventory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -13,8 +14,9 @@ import org.lwjgl.opengl.GL11;
 
 public class GuiLetter extends GuiContainer {
     private static final int BUTTON_SIGN = 1;
+    private static final int BUTTON_NEXT_PAGE = 2;
     private final EntityPlayer player;
-    private ResourceLocation background = new ResourceLocation(MailboxMod.ID, "/textures/gui/letter.png");
+    private static ResourceLocation background = new ResourceLocation(MailboxMod.ID, "/textures/gui/letter.png");
     private GuiTextField subject;
 
     public GuiLetter(EntityPlayer player) {
@@ -30,7 +32,9 @@ public class GuiLetter extends GuiContainer {
         guiTop = (int) ((height - ySize) / 2.0f);
         Keyboard.enableRepeatEvents(true);
         // add buttons
-        buttonList.add(new GuiButton(BUTTON_SIGN, guiLeft + 138, guiTop + 138, 32, 20, "Sign"));
+        buttonList.add(new GuiButton(BUTTON_SIGN, guiLeft + 137, guiTop + 137, 32, 20, "Sign"));
+        buttonList.add(new GuiLetter.NextPageButton(BUTTON_NEXT_PAGE, true, guiLeft + 136, guiTop + 124));
+        buttonList.add(new GuiLetter.NextPageButton(BUTTON_NEXT_PAGE, false, guiLeft + 40, guiTop + 124));
     }
 
     public void onGuiClosed() {
@@ -39,7 +43,7 @@ public class GuiLetter extends GuiContainer {
     }
 
     protected void actionPerformed(GuiButton guibutton) {
-        switch(guibutton.id) {
+        switch (guibutton.id) {
             case BUTTON_SIGN:
                 MailboxMod.channel.sendToServer(PacketChangeInventory.createWriteLetterPacket(player, "etxt"));
                 player.closeScreen();
@@ -52,5 +56,34 @@ public class GuiLetter extends GuiContainer {
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         mc.renderEngine.bindTexture(background);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+    }
+
+    private class NextPageButton extends GuiButton {
+        private boolean direction;
+
+        public NextPageButton(int id, boolean direction, int x, int y) {
+            super(id, x, y, 18, 10, "");
+            this.direction = direction;
+        }
+
+        @Override
+        public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+            if (this.visible) {
+                boolean flag = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                mc.getTextureManager().bindTexture(GuiLetter.background);
+                int ux = 178;
+                int uy = 0;
+                if (!direction) {
+                    // switch direction
+                    uy += 10;
+                }
+                if (flag) {
+                    // mouse over
+                    ux += 18;
+                }
+                drawTexturedModalRect(this.xPosition, this.yPosition, ux, uy, 18, 10);
+            }
+        }
     }
 }
