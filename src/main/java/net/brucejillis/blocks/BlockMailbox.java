c6cee354d38938615f849ff0763d4b474ca218f1
Jillis ter Hove <j.terhove@gmail.com>
2014-05-17 13:05:12 +0200

    autocommit day -1 13:05

--- src/main/java/net/brucejillis/blocks/BlockMailbox.java
+++ src/main/java/net/brucejillis/blocks/BlockMailbox.java
@@ -35,17 +35,11 @@ public class BlockMailbox extends BlockContainer {
     }
 
     @Override
-    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List pool, Entity entity) {
-        setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.925f, 1.0f);
-        super.addCollisionBoxesToList(world, x, y, z, aabb, pool, entity);
-    }
-
-    @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
-        TileEntity te = world.getTileEntity(x, y, z);
         if (player.isSneaking()) {
             return false;
         }
+        TileEntity te = world.getTileEntity(x, y, z);
         // try to get the mailbox tileentity
         if (te == null) {
             // try to get base block entity
@@ -56,86 +50,20 @@ public class BlockMailbox extends BlockContainer {
             }
         }
         if (te instanceof TileEntityMailbox) {
-            world.playSoundEffect((double)te.xCoord + 0.5D, (double)te.yCoord + 0.5D, (double)te.zCoord + 0.5D, MailboxMod.ID + ":mailbox_open", 1.0f, 1.0f);
+            world.playSoundEffect((double) te.xCoord + 0.5D, (double) te.yCoord + 0.5D, (double) te.zCoord + 0.5D, MailboxMod.ID + ":mailbox_open", 1.0f, 1.0f);
             // open the mailbox's gui
             player.openGui(MailboxMod.instance, MailboxMod.GUI_MAILBOX, world, x, y, z);
         }
         return true;
     }
 
-    @Override
-    public void breakBlock(World world, int x, int y, int z, Block block, int metadata) {
-        if (!isMailboxBase(metadata))
-            return;
-        TileEntity entity = world.getTileEntity(x, y, z);
-        if (entity == null || !(entity instanceof TileEntityMailbox)) {
-            return;
-        }
-        Random rand = new Random();
-        IInventory inventory = (IInventory) entity;
-        for (int i = 0; i < inventory.getSizeInventory(); i++) {
-            ItemStack stack = inventory.getStackInSlot(i);
-            if (stack != null && stack.stackSize > 0) {
-                float rx = rand.nextFloat() * 0.8F + 0.1F;
-                float ry = rand.nextFloat() * 0.8F + 0.1F;
-                float rz = rand.nextFloat() * 0.8F + 0.1F;
-                EntityItem entityItem = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(stack.getItem(), stack.stackSize, stack.getItemDamage()));
-                if (stack.hasTagCompound()) {
-                    entityItem.getEntityItem().setTagCompound((NBTTagCompound) stack.getTagCompound().copy());
-                }
-                float factor = 0.05F;
-                entityItem.motionX = rand.nextGaussian() * factor;
-                entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
-                entityItem.motionZ = rand.nextGaussian() * factor;
-                world.spawnEntityInWorld(entityItem);
-                stack.stackSize = 0;
-            }
-        }
-        super.breakBlock(world, x, y, z, block, metadata);
-    }
-
     public static boolean isMailboxBase(int metadata) {
-        if (metadata == 1) {
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onBlockHarvested(World world, int x, int y, int z, int metadata, EntityPlayer player) {
-        // erase extra block if the player harvest the primary (base)block
-        if (isMailboxBase(metadata)) {
-            if (world.getBlock(x, y, z) == this) {
-                world.setBlockToAir(x, y + 1, z);
-            }
-        } else {
-            if (world.getBlock(x, y, z) == this) {
-                world.setBlockToAir(x, y - 1, z);
-            }
-        }
-    }
-
-    @Override
-    public void setBlockBoundsBasedOnState(IBlockAccess access, int x, int y, int z) {
-        if (isMailboxBase(access.getBlockMetadata(x, y, z))) {
-            //setBlockBounds(0.1f, 0.1f, 0.1f, 0.9f, 0.9f, 0.9f);
-            setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.65f, 1.0f);
-        } else {
-            //setBlockBounds(0.1f, 0.1f, 0.1f, 0.9f, 0.9f, 0.9f);
-            setBlockBounds(0.0f, -1.0f, 0.0f, 1.0f, 0.65f, 1.0f);
-        }
+        return metadata == 1;
     }
 
     @Override
     public TileEntity createNewTileEntity(World world, int metadata) {
-        if (isMailboxBase(metadata))
-            return new TileEntityMailbox();
-        return null;
-    }
-
-    @Override
-    public int getMobilityFlag() {
-        return 1;
+        return new TileEntityMailbox();
     }
 
     @Override
@@ -157,4 +85,96 @@ public class BlockMailbox extends BlockContainer {
     public Item getItem(World world, int x, int y, int z) {
         return MailboxMod.itemMailbox;
     }
+
+//    @Override
+//    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List pool, Entity entity) {
+//        setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.925f, 1.0f);
+//        super.addCollisionBoxesToList(world, x, y, z, aabb, pool, entity);
+//    }
+//
+//    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_) {
+//        float f = 0.0625F;
+//        return AxisAlignedBB.getAABBPool().getAABB((double) ((float) p_149668_2_ + f), (double) p_149668_3_, (double) ((float) p_149668_4_ + f), (double) ((float) (p_149668_2_ + 1) - f), (double) ((float) (p_149668_3_ + 1) - f), (double) ((float) (p_149668_4_ + 1) - f));
+//    }
+//
+//    /**
+//     * Returns the bounding box of the wired rectangular prism to render.
+//     */
+//    @SideOnly(Side.CLIENT)
+//    public AxisAlignedBB getSelectedBoundingBoxFromPool(World p_149633_1_, int p_149633_2_, int p_149633_3_, int p_149633_4_) {
+//        float f = 0.0625F;
+//        return AxisAlignedBB.getAABBPool().getAABB((double) ((float) p_149633_2_ + f), (double) p_149633_3_, (double) ((float) p_149633_4_ + f), (double) ((float) (p_149633_2_ + 1) - f), (double) (p_149633_3_ + 1), (double) ((float) (p_149633_4_ + 1) - f));
+//    }
+//
+
+//
+//    @Override
+//    public void breakBlock(World world, int x, int y, int z, Block block, int metadata) {
+//        if (!isMailboxBase(metadata)) return;
+//        TileEntity entity = world.getTileEntity(x, y, z);
+//        if (entity == null || !(entity instanceof TileEntityMailbox)) {
+//            return;
+//        }
+//        Random rand = new Random();
+//        IInventory inventory = (IInventory) entity;
+//        for (int i = 0; i < inventory.getSizeInventory(); i++) {
+//            ItemStack stack = inventory.getStackInSlot(i);
+//            if (stack != null && stack.stackSize > 0) {
+//                float rx = rand.nextFloat() * 0.8F + 0.1F;
+//                float ry = rand.nextFloat() * 0.8F + 0.1F;
+//                float rz = rand.nextFloat() * 0.8F + 0.1F;
+//                EntityItem entityItem = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(stack.getItem(), stack.stackSize, stack.getItemDamage()));
+//                if (stack.hasTagCompound()) {
+//                    entityItem.getEntityItem().setTagCompound((NBTTagCompound) stack.getTagCompound().copy());
+//                }
+//                float factor = 0.05F;
+//                entityItem.motionX = rand.nextGaussian() * factor;
+//                entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+//                entityItem.motionZ = rand.nextGaussian() * factor;
+//                world.spawnEntityInWorld(entityItem);
+//                stack.stackSize = 0;
+//            }
+//        }
+//        super.breakBlock(world, x, y, z, block, metadata);
+//    }
+//
+//    public static boolean isMailboxBase(int metadata) {
+//        if (metadata == 1) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public void onBlockHarvested(World world, int x, int y, int z, int metadata, EntityPlayer player) {
+//        // erase extra block if the player harvest the primary (base)block
+//        if (isMailboxBase(metadata)) {
+//            if (world.getBlock(x, y, z) == this) {
+//                world.setBlockToAir(x, y + 1, z);
+//            }
+//        } else {
+//            if (world.getBlock(x, y, z) == this) {
+//                world.setBlockToAir(x, y - 1, z);
+//            }
+//        }
+//    }
+//
+//    @Override
+    //    public void setBlockBoundsBasedOnState(IBlockAccess access, int x, int y, int z) {
+//        if (isMailboxBase(access.getBlockMetadata(x, y, z))) {
+//            //setBlockBounds(0.1f, 0.1f, 0.1f, 0.9f, 0.9f, 0.9f);
+//            setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.65f, 1.0f);
+//        } else {
+//            //setBlockBounds(0.1f, 0.1f, 0.1f, 0.9f, 0.9f, 0.9f);
+//            setBlockBounds(0.0f, -1.0f, 0.0f, 1.0f, 0.65f, 1.0f);
+//        }
+//    }
+//
+
+//
+//    @Override
+//    public int getMobilityFlag() {
+//        return 1;
+//    }
+//
 }
