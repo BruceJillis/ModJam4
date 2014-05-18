f81f467fb061cf4a9957b867438ae3aab6191cad
Jillis ter Hove <j.terhove@gmail.com>
2014-05-18 17:03:26 +0200

    autocommit day -2 17:03

--- src/main/java/net/brucejillis/renderers/RenderBlockMailbox.java
+++ src/main/java/net/brucejillis/renderers/RenderBlockMailbox.java
@@ -11,8 +11,8 @@ import net.minecraftforge.client.model.IModelCustom;
 import org.lwjgl.opengl.GL11;
 
 public class RenderBlockMailbox extends TileEntitySpecialRenderer {
-    private final ResourceLocation modelLocation = new ResourceLocation(MailboxMod.ID, "/textures/models/mailbox.obj");
-    private final ResourceLocation textureLocation = new ResourceLocation(MailboxMod.ID, "/textures/models/mailbox.png");
+    private final ResourceLocation modelLocation = new ResourceLocation(MailboxMod.ID, "/textures/models/mailbox2.obj");
+    private final ResourceLocation textureLocation = new ResourceLocation(MailboxMod.ID, "/textures/models/Pole map.png");
 
     private final IModelCustom model;
     private final Minecraft mc;
